73a3df2c256ee64200f23e509dd4026ad5dc0a21
renaebris-eng <renaebris@gmail.com>
2025-08-25 19:08:04 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -198,7 +198,9 @@ function mapPoints(points, layers) {
   // If no layer groups exist, add the feature group (possibly clustered) to the map
   if (layers === undefined || layers.length === 0) {
     if (clusters) {
-      var clusterGroup = L.markerClusterGroup();
+      var clusterGroup = L.markerClusterGroup({
+        maxClusterRadius: 0
+      });
       clusterGroup.addLayer(group);
       map.addLayer(clusterGroup);
     } else {
@@ -207,7 +209,9 @@ function mapPoints(points, layers) {
   } else {
     if (clusters) {
       // Multilayer cluster support
-      multilayerClusterSupport = L.markerClusterGroup.layerSupport();
+      multilayerClusterSupport = L.markerClusterGroup.layerSupport({
+        maxClusterRadius: 0
+      });
       multilayerClusterSupport.addTo(map);
 
       for (var lname in layers) {
@@ -216,6 +220,7 @@ function mapPoints(points, layers) {
         layers[lname].addTo(map);
       }
     }
+  }
 
     var pos = (getSetting('_pointsLegendPos') == 'off')
       ? 'topleft'
